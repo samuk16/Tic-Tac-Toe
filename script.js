d063b28286d30c150760b2b77ccdbe5fef451c31
Samuk <marcelolamas19@gmail.com>
2022-12-05 21:05:15 -0300

    Improve the functions that create DOM elements

--- script.js
+++ script.js
@@ -1,4 +1,247 @@
+import createElementsDom from './domCreation.js';
 
+const arrDomObjGame = [
+    {
+        elementType: 'div',
+        attributes: {class:'containerTTT'},
+        appendChild: 'body',
+    },
+    //  childs containerTTT
+    {
+        elementType: 'div',
+        attributes: {class:'containerScore'},
+        appendChild: '.containerTTT',
+    },
+    {
+        elementType: 'div',
+        attributes: {class:'containerBoard'},
+        appendChild: '.containerTTT',
+    },
+    {
+        elementType: 'div',
+        attributes: {class:'containerBtns'},
+        appendChild: '.containerTTT',
+    },
+
+    // childs containerScore
+    {
+        elementType: 'div',
+        attributes: {class:'name1'},
+        appendChild: '.containerScore',
+    },
+    //  child name1
+    {
+        elementType: 'p',
+        attributes: {class:'nameScore1'},
+        appendChild: '.name1',
+    },
+
+    {
+        elementType: 'div',
+        attributes: {class:'scoreP'},
+        appendChild: '.containerScore',
+    },
+    //  child scoreP
+    {
+        elementType: 'p',
+        attributes: {class:'score'},
+        appendChild: '.scoreP',
+    },
+
+    {
+        elementType: 'div',
+        attributes: {class:'name2'},
+        appendChild: '.containerScore',
+    },
+    //  child name2
+    {
+        elementType: 'p',
+        attributes: {class:'nameScore2'},
+        appendChild: '.name2',
+    },
+
+    //  childs containerBoard
+
+    {
+        elementType: 'div',
+        attributes: {class:'boardItem'},
+        appendChild: '.containerBoard',
+    },
+    {
+        elementType: 'div',
+        attributes: {class:'boardItem'},
+        appendChild: '.containerBoard',
+    },
+    {
+        elementType: 'div',
+        attributes: {class:'boardItem'},
+        appendChild: '.containerBoard',
+    },
+    {
+        elementType: 'div',
+        attributes: {class:'boardItem'},
+        appendChild: '.containerBoard',
+    },
+    {
+        elementType: 'div',
+        attributes: {class:'boardItem'},
+        appendChild: '.containerBoard',
+    },
+    {
+        elementType: 'div',
+        attributes: {class:'boardItem'},
+        appendChild: '.containerBoard',
+    },
+    {
+        elementType: 'div',
+        attributes: {class:'boardItem'},
+        appendChild: '.containerBoard',
+    },
+    {
+        elementType: 'div',
+        attributes: {class:'boardItem'},
+        appendChild: '.containerBoard',
+    },
+    {
+        elementType: 'div',
+        attributes: {class:'boardItem'},
+        appendChild: '.containerBoard',
+    },
+
+    //  childs containerBtns
+
+    {
+        elementType: 'div',
+        attributes: {class:'btnRematch'},
+        innerText: 'Rematch' ,
+        appendChild: '.containerBtns',
+    },
+    {
+        elementType: 'div',
+        attributes: {class:'btnNewGame'},
+        innerText: 'New Game' ,
+        appendChild: '.containerBtns',
+    },
+
+];
+
+const arrDomObjStart = [
+    {
+        elementType: 'div',
+        attributes: {class:'containerStartGame'},
+        appendChild: 'body',
+    },
+
+    //  childs containerStartGame
+
+    {
+        elementType: 'form',
+        attributes: {class: 'formStartGame'},
+        appendChild: '.containerStartGame',
+    },
+
+    //  childs formStartGame
+
+    {
+        elementType: 'div',
+        attributes: {class: 'containerLabels'},
+        appendChild: '.formStartGame',
+    },
+    {
+        elementType: 'button',
+        attributes: {type: 'submit'},
+        innerText: 'Start Game' ,
+        appendChild: '.formStartGame',
+    },
+
+    //  childs containerLabels
+
+    {
+        elementType: 'label',
+        attributes: {class: 'label1'},
+        appendChild: '.containerLabels',
+    },
+    {
+        elementType: 'label',
+        attributes: {class: 'label2'},
+        appendChild: '.containerLabels',
+    },
+
+    //  childs label1
+
+    {
+        elementType: 'p',
+        attributes: null,
+        innerText: 'Player 1' ,
+        appendChild: '.label1',
+    },
+
+    {
+        elementType: 'div',
+        attributes: {class: 'containerInputs1'},
+        appendChild: '.label1',
+    },
+
+    //  childs containerInputs1
+
+    {
+        elementType: 'input',
+        attributes: {type: 'text', name: 'namePlayer1'},
+        appendChild: '.containerInputs1',
+    },
+
+    {
+        elementType: 'div',
+        attributes: {class: 'inputColor1'},
+        appendChild: '.containerInputs1',
+    },
+
+    //  child inputColor1
+
+    {
+        elementType: 'input',
+        attributes: {type: 'color', name: 'color1'},
+        appendChild: '.inputColor1',
+    },
+
+    //  childs label2
+
+    {
+        elementType: 'p',
+        attributes: null,
+        innerText: 'Player 2' ,
+        appendChild: '.label2',
+    },
+
+    {
+        elementType: 'div',
+        attributes: {class: 'containerInputs2'},
+        appendChild: '.label2',
+    },
+
+    //  childs containerInputs2
+
+    {
+        elementType: 'input',
+        attributes: {type: 'text', name: 'namePlayer2'},
+        appendChild: '.containerInputs2',
+    },
+
+    {
+        elementType: 'div',
+        attributes: {class: 'inputColor2'},
+        appendChild: '.containerInputs2',
+    },
+
+    //  child inputColor2
+
+    {
+        elementType: 'input',
+        attributes: {type: 'color', name: 'color2'},
+        appendChild: '.inputColor2',
+    },
+    
+];
 
 const gameBoard = (function(){
 
@@ -172,66 +415,13 @@ test2(test6);
 // diagonalVerification(vericationObj,boardTest)
 
 
-
-function createElementsDom(elementType,attributes,innerHTML,innerText,appendChild) {
+function domElementsGame(arr) {
     
-    if(elementType){
-        let element = document.createElement(elementType);
-  
-        if (attributes) {
-            for (const key in attributes){
-                element.setAttribute(key,attributes[key])
-            }
-        }
-
-        if (innerHTML) {
-            element.innerHTML= innerHTML;
-
-        }    
-        if (innerText) {
-            element.innerText = innerText;
-
-        }
-        if(appendChild) {
-            appendChild.appendChild(element);
-            
-        } 
-
-        return element;
-    }
+    arr.forEach(elementObject => {
     
-}
-
-function domElementsGame() {
+        createElementsDom(elementObject.elementType,elementObject.attributes,null,elementObject.innerText,document.querySelector(elementObject.appendChild));
         
-    
-
-    //  Contianer Main
-
-    createElementsDom('div',{class:'containerTTT'},null,null,document.body)
-
-    //  childs containerTTT
-
-    createElementsDom('div',{class:'containerScore'},null,null,document.querySelector('.containerTTT'));
-    createElementsDom('div',{class:'containerBoard'},null,null,document.querySelector('.containerTTT'));
-    createElementsDom('div',{class:'containerBtns'},null,null,document.querySelector('.containerTTT'));
-
-    //  Child ContainerScore
-
-    createElementsDom('div',{class:'containerName1'},null,null,document.querySelector('.containerScore'));
-    createElementsDom('p',null,null,'Goku',document.querySelector('.containerName1'));
-
-    createElementsDom('div',{class:'containerScoreP'},null,null,document.querySelector('.containerScore'));
-    createElementsDom('p',null,null,'1 - 1',document.querySelector('.containerScoreP'));
-
-    createElementsDom('div',{class:'containerName2'},null,null,document.querySelector('.containerScore'));
-    createElementsDom('p',null,null,'Gohan',document.querySelector('.containerName2'));
-
-    // Childs ContainerBoard
-    
-    for (let i = 0; i <= 8; i++) {
-        createElementsDom('div',{class:'boardItem'},null,null,document.querySelector('.containerBoard'));
-    }
+    })
 
     const boardItem = document.querySelectorAll('.boardItem');
 
@@ -242,183 +432,23 @@ function domElementsGame() {
         item.style.animation = 'animationOutline 0.2s linear both' ;
     
     }))
-
-    //  Childs containerBtns
-
-    createElementsDom('div',null,null,'Rematch',document.querySelector('.containerBtns'));
-    createElementsDom('div',null,null,'New Game',document.querySelector('.containerBtns'));
-
-
 }
 
-domElementsGame();
-
-function domElementsMenu() {
-    
-    //  containerMain
-
-    createElementsDom('div',{class:'containerStartGame'},null,null,document.body);
-
-    //  child containerStartGame
-
-    createElementsDom('form',{class:'formPlayers'},null,null,document.querySelector('.containerStartGame'));
-    
-    //  childs formPlayers
-    
-    createElementsDom('div',{class:'containerLabels'},null,null,document.querySelector('.formPlayers'));
-
-    createElementsDom('button',{type: 'submit'},null,'Start Game',document.querySelector('.formPlayers'));
-
-    //  child containerLabels
-
-    createElementsDom('label',{class: 'label1'},null,null,document.querySelector('.containerLabels'));
-    
-    createElementsDom('label',{class: 'label2'},null,null,document.querySelector('.containerLabels'));
-    
-    //  cholds label1
-
-    createElementsDom('p',null,null,'Player 1',document.querySelector('.label1'));
-    
-    createElementsDom('div',{class: 'containerInputs'},null,null,document.querySelector('.label1'));
-
-    //  childs containerInputs
-
-    createElementsDom('input',{type: 'text', name:'namePlayer'},null,null,document.querySelector('.containerInputs'));
-
-    createElementsDom('div',{class: 'inputColor'},null,null,document.querySelector('.containerInputs'));
-
-    //  child inputColor
-
-    createElementsDom('input',{type: 'color',name:'color'},null,null,document.querySelector('.inputColor'));
-
-    //  cholds label2
-
-    createElementsDom('p',null,null,'Player 2',document.querySelector('.label2'));
-
-    createElementsDom('div',{class: 'containerInputs2'},null,null,document.querySelector('.label2'));
-
-    //  childs containerInputs
-
-    createElementsDom('input',{type: 'text', name:'namePlayer2'},null,null,document.querySelector('.containerInputs2'));
-
-    createElementsDom('div',{class: 'inputColor2'},null,null,document.querySelector('.containerInputs2'));
-
-    //  child inputColor
-    
-    createElementsDom('input',{type: 'color',name:'color2'},null,null,document.querySelector('.inputColor2'));
-    
-
-}
-
-// domElementsMenu()
-
-function createElementsDom2(arr) {
-    
+function domElementsMenu(arr) {
     
     arr.forEach(elementObject => {
-
-        if(elementObject.elementType){
-            let element = document.createElement(elementObject.elementType);
-            // console.log(elementObject.elementType);
+    
+        createElementsDom(elementObject.elementType,elementObject.attributes,null,elementObject.innerText,document.querySelector(elementObject.appendChild));
         
-            if (elementObject.attributes) {
-
-                // let test5 = elementObject.attributes;
-                
-                for (const key in elementObject.attributes){
-                    element.setAttribute(key,elementObject.attributes[key])
-                }
-                // console.log('atributos');
-            }
-
-            // if (innerHTML) {
-            //     element.innerHTML= innerHTML;
-
-            // }    
-            if (elementObject.innerText) {
-
-                element.innerText = elementObject.innerText;
-                // console.log('text');
-
-            }
-            if(elementObject.appendChild) {
-
-                elementObject.appendChild.appendChild(element);
-                console.log(elementObject.appendChild);
-                
-
-            } 
-
-            return element;
-        }
     })
 
-
-        
-
-    
-    
 }
 
 
-let objDomTest = {
 
-    containerTTT: {
-        elementType: 'div',
-        attributes: {class:'containerTTT'},
-        appendChild: document.body,
-    },
-        containerScore: {
-            elementType: 'div',
-            attributes: {class:'containerScore'},
-            appendChild: document.querySelector('.containerTTT'),
-        },
-
-        child1:{
-            elementType: 'div',
-            appendChild: document.querySelector('.containerScore'),
-        },
-        child2:{
-            elementType: 'div',
-            appendChild: document.querySelector('.containerScore'),
-        },
-        child3:{
-            elementType: 'div',
-            appendChild: document.querySelector('.containerScore'),
-        },
-
-    containerBoard:{
-        elementType: 'div',
-        attributes: {class:'containerBoard'},
-        appendChild: document.querySelector('.containerTTT'),
-    }
-       
-
-};
-
-let arrObjTest = [
-    {
-        elementType: 'div',
-        attributes: {class:'containerTTT',id:'TTT'},
-        appendChild: document.body,
-    },
-    {
-        elementType: 'div',
-        attributes: {class:'containerScore'},
-        appendChild: document.getElementById('TTT'),
-    },
-    {
-        elementType: 'div',
-        appendChild: document.getElementById('TTT'),
-    },
-
-];
-
-// createElementsDom(arrObjTest);
-
-// console.log(arrObjTest[1]);
-// console.log(objDomTest.containerScore.appendChild);
+// domElementsGame(arrDomObjGame);
+domElementsMenu(arrDomObjStart);
 
 
-// createElementsDom('div',{'data-id':'1',class:'nashe',style:'background-color: violet',},null,'hola',document.body);
-// createElementsDom('div',{'data-id':'2',class:'nashe2',},null,'hola2',document.querySelector('.nashe'));
+
+
